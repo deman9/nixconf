efd414e920041324b3e95380c1df17315e676cf6
deman9 <andem@tuta.io>
2025-08-04 19:27:33 +0200

    lsp config nvim

--- modules/nvim/plugin/lsp.lua
+++ modules/nvim/plugin/lsp.lua
@@ -123,28 +123,25 @@ require('lspconfig').lua_ls.setup {
     },
 }
 
-require('lspconfig').nixd.setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-	settings = {
-		nixd = {
-		  nixpkgs = {
-			expr = "import <nixpkgs> { }",
-		  },
-		  formatting = {
-			command = { "nixfmt" }, -- or nixfmt or nixpkgs-fmt
-		  },
-		  -- options = {
-		  --   nixos = {
-		  --       expr = '(builtins.getFlake "/PATH/TO/FLAKE").nixosConfigurations.CONFIGNAME.options',
-		  --   },
-		  --   home_manager = {
-		  --       expr = '(builtins.getFlake "/PATH/TO/FLAKE").homeConfigurations.CONFIGNAME.options',
-		  --   },
-		  -- },
-		},
-	  },
-}
+      require('lspconfig').nixd.setup{}
+      require('lspconfig')['nixd'].setup {
+        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+      }-- require('lspconfig').nixd.setup {
+--     on_attach = on_attach,
+--   require('lspconfig')['nixd'].setup {
+--    capabilities = require("cmp_nvim_lsp").default_capabilities(),
+--       }
+-- 	settings = {
+-- 		nixd = {
+-- 		  nixpkgs = {
+-- 			expr = "import <nixpkgs> { }",
+-- 		  },
+-- 		  formatting = {
+-- 			command = { "nixfmt" }, -- or nixfmt or nixpkgs-fmt
+-- 		  },
+-- 		},
+-- 	  },
+-- }
 
 require('lspconfig').gopls.setup {
     on_attach = on_attach,
--- modules/nvim_lazy/nvim_lazy.nix
+++ modules/nvim_lazy/nvim_lazy.nix
@@ -18,13 +18,19 @@ programs.neovim = {
 	python312Packages.python-lsp-server
 	wl-clipboard
 	fd
+	gcc
   ];
 
   plugins = with pkgs.vimPlugins; [
    lazy-nvim
+   gitsigns-nvim
    which-key-nvim
+   onenord-nvim
    nvim-treesitter
+   conform-nvim
+   blink-cmp
 	mini-pairs
+	mini-ai
 	mini-surround
 	mini-comment
 	mini-bracketed
@@ -43,6 +49,11 @@ programs.neovim = {
 	cmp-path
 	luasnip
 	friendly-snippets
+	nvim-lint
+	  nvim-treesitter-context
+	  nvim-ts-autotag
+	  nvim-treesitter-textobjects
+	  nvim-treesitter.withAllGrammars
   ];
 
   extraLuaConfig = ''
@@ -60,18 +71,27 @@ programs.neovim = {
 	vim.opt.timeoutlen = 300
 	vim.opt.undofile = true
 	vim.opt.breakindent = true
-	vim.opt.showmode = false
+	vim.opt.showmode = true
 	vim.opt.list = true
 	vim.opt.inccommand = 'split'
 	vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 	vim.opt.cursorline = true
 	vim.opt.scrolloff = 10
 	vim.opt.hlsearch = true
+	vim.lsp.enable({"nixd", "lua_ls"})
 	require('mini.surround').setup()
 	require('mini.pairs').setup()
 	require('mini.comment').setup()
 	require('mini.ai').setup()
 	require('mini.bracketed').setup()
+
+	vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+	vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+	vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+	vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+	vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+	vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+	vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 	vim.api.nvim_create_autocmd('TextYankPost', {
 	  desc = 'Highlight when yanking (copying) text',
@@ -80,8 +100,6 @@ programs.neovim = {
 		vim.highlight.on_yank()
 	  end,
 	})
-
-
 
     require("lazy").setup({
       performance = {
@@ -102,9 +120,6 @@ programs.neovim = {
         { import = "plugins" },
       },
     })
-
-
-
   '';
   };
   xdg.configFile."nvim/lua" = {
